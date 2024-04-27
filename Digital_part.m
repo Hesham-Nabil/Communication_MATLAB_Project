42a4f8d24dec48fe326f6cf0bb76743de81ac3d2
Mohamed-elmoner <mohamed.elmoner.12@gmail.com>
2024-04-28 00:08:16 +0200

    Digital Edit

--- Digital_part.m
+++ Digital_part.m
@@ -2,7 +2,8 @@ clc;
 clear;
 close all
 bits = 64;
-stream = randi([0, 1], 1, bits);
+stream =randi([0, 1], 1, bits);
+
 ts = bits;
 T = 100*bits;
 t = 0 : 1 : (100*bits-1);
@@ -14,7 +15,7 @@ bipolar = zeros(size(t));
 flag = 1;  % Start with +ve pulse
 for i = 1:bits
     if stream(i) == 1
-        flag = -flag;  % Change sign
+       flag=-flag ;
        bipolar((i-1)*100+1:i*100) = flag;
     end
 end
